c7b383830d1c29b6dc68a0d91c494ae241b7d69b
Julia <jyan@verto.ca>
2017-05-10 08:52:14 -0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,70 +3,63 @@ require 'spec_helper'
 
 describe "Static pages" do
   let (:title) {"Ruby on Rails Tutorial Sample App | "}
+    subject { page }
 
 
   # Below is decribing the home page and what it should contain
   describe "Home page" do
+    # visit the page before hand
+    before { visit root_path }
+
     # The 'it' line describes to readers what the page should have, it is irrelevent
     # to the actual code 
-    it "should have the content 'Sample App'" do
+    #it "should have the content 'Sample App'" do
       #Here we go to the approprite page and then check the contents of it
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App') 
-    end
+      #visit root_path
+      #expect(page).to have_content('Sample App') 
+    #end
+    it { should have_content('Sample App') }
 
     #Checks that the home page has the approprite title, checks the contents inside 
     #of  the <title> tag. have_title checks substring so the title does no have to 
     #be an exact match
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
-    end
+    #it "should have the base title" do
+      #visit root_path
+      #expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    #end
+    it { should have_title(full_title('')) }
 
     #Making sure the title is not a custom one, we are using substrings
     #for this test case 
-    it "should not have a custom title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
+    #it "should not have a custom title" do
+      #visit root_path
+      #expect(page).not_to have_title('| Home')
+    #end
+    it { should_not have_title('| Home') }
   end
 
   #Below is describign the Help page
   describe "Help page" do 
-    # When given an error, will use this line to describe what hte pag should contain
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
+    before { visit help_path }
 
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("#{title}Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end 
 
   #Below is describing the About page
   describe "About page" do 
-    it "should have the content 'About Us'" do 
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
+    
+    before { visit about_path }
 
-    it "should have the title 'About'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("#{title}About")
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About')) }
   end 
 
   describe 'Contact page' do
-    it "should have the content 'Contact" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('Contact')
-    end
+    
+    before { visit contact_path }
 
-    it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("#{title}Contact")
-    end
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
---
+++ spec/support/utilities.rb
@@ -0,0 +1,9 @@
+# Function that does the same thing as the full_title function from before
+def full_title(page_title)
+  base_title = "Ruby on Rails Tutorial Sample App"
+  if page_title.empty?
+    base_title
+  else
+    "#{base_title} | #{page_title}"
+  end
+end
